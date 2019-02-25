70f0181a959b6bb6b18caecfac2ae681b0c8789f
zaadie <saadiyafourie@gmail.com>
2019-02-25 12:59:09 +1100

    Sweet little Java number magic trick

---
+++ Magic.java
@@ -0,0 +1,21 @@
+public class Magic {
+	public static void main(String[] args) {
+
+		int myNumber = 8;
+    
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive/6;
+    
+        System.out.println(myNumber);
+        System.out.println(stepOne);
+        System.out.println(stepTwo);
+        System.out.println(stepThree);
+        System.out.println(stepFour);
+        System.out.println(stepFive);
+        System.out.println(stepSix);
+	}
+}
